a588a31a2e49dd5c931a50f1a9d1cc97bd433600
Khanh <khanhtranngoccva@gmail.com>
2022-05-28 22:57:50 +0700

    File input clears after file has been supplied. Enable rewind and fast forward

--- src/controlPanel.js
+++ src/controlPanel.js
@@ -1,13 +1,17 @@
 import {toggleParallax} from "./DimensionCSS.js";
 
-const browseButton = document.querySelector("#browseMIDI");
+const browseButtons = document.querySelectorAll(".browseMIDI");
 const browseButtonHidden = document.querySelector("#browseMIDIHidden");
-browseButton.addEventListener("click", function() {
+
+browseButtons.multiAssign("click", function() {
     browseButtonHidden.click();
 });
+
 browseButtonHidden.addEventListener("input", async function() {
     console.log(this.files);
+    if (this.files.length === 0) return;
     const curFile = this.files[0];
+    this.value = "";
     const uploadData = new FormData();
     uploadData.set("midiFile", curFile);
 
@@ -22,26 +26,41 @@ browseButtonHidden.addEventListener("input", async function() {
     document.dispatchEvent(newMidiEvent);
 });
 
-const togglePauseButton = document.querySelector("#toggleMIDIPause");
-togglePauseButton.addEventListener("click", function() {
+const togglePauseButton = document.querySelectorAll(".toggleMIDIPause");
+togglePauseButton.multiAssign("click", function() {
     const evt = new Event("toggleMIDIPause");
     document.dispatchEvent(evt);
 });
 
-const stopButton = document.querySelector("#stopMIDI");
-stopButton.addEventListener("click", function() {
+const stopButtons = document.querySelectorAll(".stopMIDI");
+stopButtons.multiAssign("click", function() {
     const evt = new Event("stopMIDI");
     document.dispatchEvent(evt);
 });
 
-const toggleParallaxButton = document.querySelector("#toggleParallax");
-toggleParallaxButton.addEventListener("click", function() {
+const toggleParallaxButtons = document.querySelectorAll(".toggleParallax");
+toggleParallaxButtons.multiAssign("click", function() {
     toggleParallax();
 });
 
-const perfModeButton = document.querySelector("#perfModeButton");
-perfModeButton.addEventListener("click", function() {
-    document.querySelector(".canvas").classList.toggle("hideHeavyVisuals");
+const perfModeButtons = document.querySelectorAll(".perfModeButton");
+perfModeButtons.multiAssign("click", function() {
+    document.body.classList.toggle("hideHeavyVisuals");
+});
+
+const reverseButtons = document.querySelectorAll(".rewind");
+reverseButtons.multiAssign("click", function() {
+    const evt = new Event("rewind");
+    document.dispatchEvent(evt);
+});
+
+const fastForwardButtons = document.querySelectorAll(".fastForward");
+fastForwardButtons.multiAssign("click", function() {
+    const evt = new Event("fastForward");
+    document.dispatchEvent(evt);
 });
 
 // Performance mode on by default if Chrome. Will be removed as soon as I can fix the 1400 elements lagging the app.
+if (navigator.userAgent.match(/chrome/gi)) {
+    document.body.classList.add("hideHeavyVisuals");
+}

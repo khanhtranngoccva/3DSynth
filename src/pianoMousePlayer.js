2c3d67b518e603541a5f9812ddfa1c5c72a37de5
Khanh <khanhtranngoccva@gmail.com>
2022-05-28 22:59:41 +0700

    Removed redundant prototype methods

--- src/pianoMousePlayer.js
+++ src/pianoMousePlayer.js
@@ -1,20 +1,5 @@
 import {pianoSynth} from "./pianoSynth";
 
-HTMLElement.prototype.delegate = function(eventType, childSelector, callback) {
-    this.addEventListener(eventType, function(e) {
-        const targetElement = e.target;
-        const closestParentElement = targetElement.closest(childSelector);
-        if (closestParentElement && this.contains(closestParentElement)) {
-            callback.call(closestParentElement, e);
-        }
-    });
-};
-
-NodeList.prototype.multiAssign = function(eventType, callback) {
-    Array.from(this).forEach(element => {
-        element.addEventListener(eventType, callback);
-    });
-};
 
 (function() {
     let mousedown = 0;
